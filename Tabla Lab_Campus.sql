7dfa5662184cb6e3ef12def377c469a92e6ba7b2
ANA FERNANDA GUTIERREZ VILLANUEVA <anafernandagv@comunidad.unam.mx>
2021-08-05 00:40:15 -0500

    Tabla Lab

---
+++ Tabla Lab_Campus.sql
@@ -0,0 +1,12 @@
+-- insert tabla Lab_campus 
+INSERT INTO Lab_campus VALUES ('19', 'Laboratorio L3', '411'),
+('45', 'UPIICSA Zacatenco', '678'),
+('12', 'Laboratorio de fisica', '12'),
+('21', 'UPIICSA Zacatecas', '678'),
+('16', 'Laboratorio LACE', '23'),
+('18', 'Laboratorio L4', '411'),
+('23', 'UPIICSA Hawai', '678'),
+('04', 'Laboratory C203', '3512'),
+('51', 'Laboratorio de ciencias', '11'),
+('11', 'Laboratorio L1', '411'),
+('42', 'UPIICSA Zacatenco', '678');
