4581b9a910a91ca422073ce1a71c2f8ddca6afd2
ANA FERNANDA GUTIERREZ VILLANUEVA <anafernandagv@comunidad.unam.mx>
2021-08-04 23:32:16 -0500

    Tema Revista

---
+++ Tabla TemasRevista.sql
@@ -0,0 +1,15 @@
+select * from temasrevista;
+
+insert into temasrevista values
+('1','Investigacion'),
+('2','Tecnico-Profesional'),
+('3','Divulgacion'),
+('4', 'Cultural'),
+('5', 'Multidisciplina');
+
+insert into temasrevista values
+('6','Artes y Humanidades'),
+('7','Biologia y quimica'),
+('8','Biotecnologia'),
+('9', 'Matematicas'),
+('10', 'Medicina');
