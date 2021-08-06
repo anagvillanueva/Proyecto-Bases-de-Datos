17679187d8852cdf290b88a05b8d456962967b8c
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2021-08-05 21:07:19 -0500

    Add files via upload

--- Tabla Congreso_Articulo.sql
+++ Tabla Congreso_Articulo.sql
@@ -3,8 +3,6 @@ insert into congreso_articulo values
 (NULL,3,5),
 (NULL,2,9),
 (NULL,1,14),
-(NULL,4,5),
-(NULL,5,14),
+(NULL,4,7),
 (NULL,10,6);
 
-select * from congreso_articulo;
--- Tablas Congreso - Pais.sql
+++ Tablas Congreso - Pais.sql
@@ -22,8 +22,4 @@ INSERT INTO congreso VALUES
 ('3', 'Congreso Nacional de Ciencias del Suelo','37','Zacatecas','2013-08-11','2013-08-12','Nacional','Anual','680','1'),
 ('4', 'Congreso Internacional de Medicina','6','Lima','2021-04-27','2021-04-30','Internacional','Anual','3900','3'),
 ('5', 'Congreso de Tecnologia y Negocios ','3','Santiago','2017-06-12','2017-07-12','Internacional','Bianual','1500','4'),
-('6', 'Congreso de Minerales ','78','Florencia','2019-11-12','2019-11-14','Internacional','Semestral','7800','13'),
-('7', 'Congreso de Doctores - Cardiovascular','5','Yunnan','2009-01-02','2009-01-03','Internacional','Bianual','900','10'),
-('8', 'Congreso Nacional de Ingenieria','17','Ciudad de Mexico','2017-05-22','2017-05-25','Nacional','Trimestral','789','1'),
-('9', 'Congreso de Nanotecnologia y Ciencias A.','50','Tokio','2019-01-07','2019-01-09','Internacional','Anual','2600','15'),
 ('10', 'Congreso de Karlsruhe','17','Munich','2020-07-02','2020-07-04','Internacional','Semestral','1290','12');
